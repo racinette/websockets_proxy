749581e4dc389cbcf78a8a3841b0060983f712bb
Andrei <verolomnyy@gmail.com>
2024-11-10 02:11:22 +0300

    fixed attribute error __await_impl_timeout__ for newer websockets versions

--- websockets_proxy/websockets_proxy.py
+++ websockets_proxy/websockets_proxy.py
@@ -1,23 +1,25 @@
 from typing import Optional, Union
 from urllib.parse import urlparse
 
-from websockets.legacy.client import connect
+from websockets.legacy.client import Connect, WebSocketClientProtocol
 from python_socks.async_.asyncio import Proxy
 
 
-class ProxyConnect(connect):
+class ProxyConnect(Connect):
     def __init__(  # noqa
-            self,
-            uri: str,
-            *,
-            proxy: Optional[Proxy],
-            proxy_conn_timeout: Optional[Union[int, float]] = None,
-            **kwargs
+        self,
+        uri: str,
+        *,
+        proxy: Optional[Proxy],
+        proxy_conn_timeout: Optional[Union[int, float]] = None,
+        **kwargs,
     ) -> None:
         self.uri = uri
         # This looks strange, but
         if "sock" in kwargs:
-            raise ValueError("do not supply your own 'sock' kwarg - it's used internally by the wrapper")
+            raise ValueError(
+                "do not supply your own 'sock' kwarg - it's used internally by the wrapper"
+            )
         kwargs.pop("host", None)
         kwargs.pop("port", None)
         u = urlparse(uri)
@@ -34,29 +36,38 @@ class ProxyConnect(connect):
             else:
                 raise ValueError("unknown scheme")
         self.__proxy: Proxy = proxy
-        self.__proxy_conn_timeout: Optional[Union[int, float]] = proxy_conn_timeout
+        self.__proxy_conn_timeout: Optional[Union[int, float]] = (
+            proxy_conn_timeout
+        )
         self.__host: str = host
         self.__port: int = port
         # pass it to the super().__init__ call
         self.__kwargs = kwargs
-        # We deliberately don't call the super().__init__ constructor method YET!
+        # HACK: We deliberately don't call
+        # the super().__init__ constructor method YET!
 
     def set_proxy(self, proxy: Proxy) -> None:
         self.__proxy = proxy
 
-    async def __await_impl_proxy__(self):
+    async def __await_impl_proxy__(self) -> WebSocketClientProtocol:
         # creating patched socket
         sock = await self.__proxy.connect(
             dest_host=self.__host,
             dest_port=self.__port,
-            timeout=self.__proxy_conn_timeout
+            timeout=self.__proxy_conn_timeout,  # type: ignore
         )
         self.__kwargs["sock"] = sock
         # HACK: THE super().__init__ IS DELIBERATELY CALLED HERE!
-        # It is because we need an already connected socket object inside the constructor,
+        # It is because we need an already
+        # connected socket object inside the constructor,
         # but we've only just got it inside of this method
         super().__init__(self.uri, **self.__kwargs)  # noqa
-        proto = await super().__await_impl_timeout__()
+        try:
+            await_impl = getattr(self, "__await_impl_timeout__")
+        except AttributeError:
+            # newer versions removed the __await_impl_timeout__ method
+            await_impl = getattr(self, "__await_impl__")
+        proto = await await_impl()
         return proto
 
     def __await__(self):
